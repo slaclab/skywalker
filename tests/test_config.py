1ea6f1e7110e2a2e2e3add647e73c3417029f066
teddyrendahl <trendahl@slac.stanford.edu>
2017-10-03 14:06:03 -0700

    TST: Simpler happi setup for tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -12,7 +12,13 @@ import pytest
 ##########
 # Module #
 ##########
+import pcdsdevices
+from pcdsdevices.sim.pim import PIM
 from skywalker.config import ConfigReader, sim_config
+from pcdsdevices.sim.pv import using_fake_epics_pv
+
+#Hack to use simulated PIM
+pcdsdevices.PIM = PIM
 
 def make_test_path(path):
     """
@@ -20,6 +26,7 @@ def make_test_path(path):
     """
     return os.path.join(os.path.dirname(__file__), path)
 
+@using_fake_epics_pv
 def test_system_loading():
     #Load configuration
     cfg = ConfigReader(make_test_path('happi.json'),
@@ -32,3 +39,11 @@ def test_system_loading():
     assert all([_type in system.keys() for _type in cfg.device_types])
     #Check that a second call only retrieves a cached system
     assert id(cfg.get_subsystem('m1h')) == id(system)
+
+@using_fake_epics_pv
+def test_lightpath_loading():
+    cfg = ConfigReader(make_test_path('happi.json'),
+                       make_test_path('system.json'))
+    #Load devices
+    devs = cfg.load_configuration()
+    assert len(devs) == 3
